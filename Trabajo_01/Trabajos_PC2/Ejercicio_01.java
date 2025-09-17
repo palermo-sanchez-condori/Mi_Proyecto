6bbbc5f10f84f7703a58eaa3127d39135670e556
palermosanchez <palermosanchezcondori@gmail.com>
2025-09-16 22:40:53 -0500

    nuevo trabajo

---
+++ Trabajo_01/Trabajos_PC2/Ejercicio_01.java
@@ -0,0 +1,7 @@
+/*tablas hash
+  Las tablas hash son estructuras de datos que permiten almacenar y recuperar información de manera eficiente.
+  Utilizan una función hash para calcular un índice en una tabla, donde se almacena el valor asociado a una clave.
+  Esto permite un acceso rápido a los datos, ya que la búsqueda se realiza en tiempo constante en promedio.
+  Sin embargo, el rendimiento puede verse afectado por colisiones, que ocurren cuando dos claves diferentes generan el mismo índice.
+  Para manejar colisiones, se pueden utilizar técnicas como la encadenamiento o la direccionamiento abierto.
+*/
