3a5f6f7a442f0ad192a2fea13327cfb218739ab9
palermosanchez <palermosanchezcondori@gmail.com>
2025-09-16 22:44:16 -0500

    agregacion

--- Trabajo_01/Trabajos_PC2/Ejercicio_01.java
+++ Trabajo_01/Trabajos_PC2/Ejercicio_01.java
@@ -5,3 +5,8 @@
   Sin embargo, el rendimiento puede verse afectado por colisiones, que ocurren cuando dos claves diferentes generan el mismo índice.
   Para manejar colisiones, se pueden utilizar técnicas como la encadenamiento o la direccionamiento abierto.
 */
+/* Ejemplos de funciones hash comunes son:
+  - Función hash de división: calcula el índice como el resto de la división de la clave por el tamaño de la tabla.
+  - Función hash multiplicativa: utiliza una constante fraccional para multiplicar la clave y luego extrae la parte fraccional para determinar el índice.
+  - Función hash universal: selecciona una función hash al azar de una familia de funciones para minimizar las colisiones.
+*/
