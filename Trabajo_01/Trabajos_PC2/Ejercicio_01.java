0bce078fa8186069722418ea8930c8177d76bce7
palermosanchez <palermosanchezcondori@gmail.com>
2025-09-17 19:56:38 -0500

    nuevo concepto

--- Trabajo_01/Trabajos_PC2/Ejercicio_01.java
+++ Trabajo_01/Trabajos_PC2/Ejercicio_01.java
@@ -10,3 +10,12 @@
   - Función hash multiplicativa: utiliza una constante fraccional para multiplicar la clave y luego extrae la parte fraccional para determinar el índice.
   - Función hash universal: selecciona una función hash al azar de una familia de funciones para minimizar las colisiones.
 */
+/*
+ * Funciones de Dispersión (Hash Functions) están diseñadas para distribuir uniformemente los datos en una tabla hash.
+ * Una buena función de dispersión debe minimizar las colisiones y ser rápida de calcular.
+ * Algunas características de una buena función de dispersión incluyen:
+ * - Uniformidad: distribuye las claves de manera uniforme en la tabla.
+ * - Determinismo: la misma clave siempre produce el mismo índice.
+ * - Eficiencia: debe ser rápida de calcular para no afectar el rendimiento de la tabla hash.
+ * - Simplicidad: la función debe ser fácil de implementar y entender.
+ */
