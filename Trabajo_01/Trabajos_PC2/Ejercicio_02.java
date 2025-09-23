fa5856783c2ebe994da7e5c758f66bac37f5771e
palermosanchez <palermosanchezcondori@gmail.com>
2025-09-22 22:01:26 -0500

    ejercicio 2

---
+++ Trabajo_01/Trabajos_PC2/Ejercicio_02.java
@@ -0,0 +1,25 @@
+/*
+ * Realización de una Tabla Hash
+ * Implementar una tabla hash con una función de dispersión simple y manejo de colisiones mediante direccionamiento abierto.
+ * El programa debe permitir insertar, buscar y mostrar los elementos en la tabla.
+ *  Utilizar un tamaño fijo para la tabla hash y manejar colisiones mediante sondeo lineal.
+ * Incluir comentarios que expliquen las características de una buena función de dispersión.
+ * Características de una buena función de dispersión:
+ * - Uniformidad: Distribuye las claves de manera uniforme en la tabla para minimizar colisiones.
+ * - Determinismo: La misma clave siempre produce el mismo índice.
+ * - Eficiencia: Debe ser rápida de calcular para no afectar el rendimiento de la tabla hash.
+ * - Simplicidad: La función debe ser fácil de implementar y entender.
+ * Ejemplo de función de dispersión simple: función de módulo (clave % tamaño de la tabla).
+ * Ejemplo de manejo de colisiones: sondeo lineal (buscar la siguiente celda disponible).
+ * Ejemplo de uso:
+ * - Insertar claves: 10, 20, 30, 25
+ *  Buscar claves: 20 (debe devolver true), 15 (debe devolver false)
+ * - Mostrar la tabla hash después de las inserciones.
+ * Nota: Asegúrese de incluir comentarios en el código que expliquen cómo funciona la función de dispersión y cómo se manejan las colisiones.
+ * 
+ * 
+ */
+
+public class Ejercicio_02 {
+    
+}
