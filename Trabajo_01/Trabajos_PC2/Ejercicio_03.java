01ab5b5597b43005223027363b06c40c7fe529a2
palermosanchez <palermosanchezcondori@gmail.com>
2025-09-30 17:18:43 -0500

    avance

--- Trabajo_01/Trabajos_PC2/Ejercicio_03.java
+++ Trabajo_01/Trabajos_PC2/Ejercicio_03.java
@@ -7,6 +7,16 @@
  * 5. Búsqueda exponencial
  * 6. Búsqueda ternaria
  * 7. Búsqueda por Fibonacci
+ * busueda secuencial es la mas sencilla y basica ya que funciona de la siguiente manera:
+ * se recorre el arreglo desde el primer elemento hasta el ultimo elemento
+ * y se compara cada elemento con el elemento que se esta buscando
+ * si se encuentra el elemento se retorna la posicion del elemento
+ * si no se encuentra el elemento se retorna -1
+ * si el arreglo esta ordenado se puede utilizar la busqueda binaria que es mas eficiente
+ * ya que divide el arreglo en dos partes y se compara el elemento con el elemento del medio
+ * si el elemento es menor que el elemento del medio se busca en la primera mitad
+ * si el elemento es mayor que el elemento del medio se busca en la segunda mitad
+ * y asi sucesivamente hasta encontrar el elemento o hasta que el arreglo no se pueda dividir mas
  */
 
  //me falta agregar el codigo para los algoritomos de busqueda externa
