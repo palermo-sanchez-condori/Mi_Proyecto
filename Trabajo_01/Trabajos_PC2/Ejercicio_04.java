f16dd6c1ea09fcd32a19815150491fec59612511
palermosanchez <palermosanchezcondori@gmail.com>
2025-10-06 11:06:14 -0500

    funcionalidad

--- Trabajo_01/Trabajos_PC2/Ejercicio_04.java
+++ Trabajo_01/Trabajos_PC2/Ejercicio_04.java
@@ -9,6 +9,16 @@
  * rápidas y eficientes, ya que en lugar de buscar en todos los documentos, se puede
  * ir directamente a la lista de documentos asociados con el término buscado.
  * 
+ * funcionalidad:
+ * 1. Indexación: Durante el proceso de indexación, se analiza el contenido de
+ * los documentos y se crea una lista invertida que mapea cada término a los
+ * documentos en los que aparece.
+ * 2. Búsqueda: Cuando un usuario realiza una búsqueda, el sistema consulta la
+ * lista invertida para encontrar rápidamente los documentos relevantes que
+ * contienen los términos buscados.
+ * 3. Eficiencia: Las listas invertidas son eficientes en términos de espacio y
+ * tiempo, ya que permiten realizar búsquedas rápidas sin necesidad de escanear
+ * todos los documentos.
  */
 //me falta agregar el codigo
 //me falta el enlace del vidde
