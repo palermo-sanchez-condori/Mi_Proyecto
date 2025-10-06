5c9d5f7a993a2c7f06b7b1387d71d970cea14805
palermosanchez <palermosanchezcondori@gmail.com>
2025-10-05 22:30:40 -0500

    actualizacion

--- Trabajo_01/Trabajos_PC2/Ejercicio_04.java
+++ Trabajo_01/Trabajos_PC2/Ejercicio_04.java
@@ -1,3 +1,14 @@
 //me falta agregar concepto  y un ejemplo
+/*
+ * Busqueda externa Listas Invertidas son estructuras de datos que permiten
+ * almacenar y buscar información de manera eficiente, especialmente en grandes
+ * conjuntos de datos. Estas listas se utilizan comúnmente en motores de búsqueda,
+ * bases de datos y sistemas de recuperación de información.
+ * Una lista invertida asocia cada término o palabra clave con una lista de
+ * documentos o registros donde aparece ese término. Esto permite realizar búsquedas
+ * rápidas y eficientes, ya que en lugar de buscar en todos los documentos, se puede
+ * ir directamente a la lista de documentos asociados con el término buscado.
+ * 
+ */
 //me falta agregar el codigo
 //me falta el enlace del vidde
