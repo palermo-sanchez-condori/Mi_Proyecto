a934903b3a4ab683b27d91ca732b8a9762759afd
palermosanchez <palermosanchezcondori@gmail.com>
2025-10-13 11:29:08 -0500

    Multilista

--- Trabajo_01/Trabajos_PC2/Ejercicio_05.java
+++ Trabajo_01/Trabajos_PC2/Ejercicio_05.java
@@ -1,3 +1,19 @@
 //agregar codigo
 //me falta agregar concepto  y un ejemplo
+/*
+ * busqueda externa Multilistas son:
+ * estructuras de datos que permiten organizar y gestionar grandes conjuntos de datos que no caben en la memoria principal, utilizando múltiples listas enlazadas para almacenar y acceder a los datos de manera eficiente.
+ * Estas estructuras son especialmente útiles para manejar datos que residen en dispositivos de almacenamiento externo, como discos duros o bases de datos, donde el acceso a los datos es más lento que en la memoria principal.
+ * Las multilistas permiten dividir los datos en varias listas enlazadas, cada una de las cuales puede estar organizada de diferentes maneras (por ejemplo, por categorías, rangos de valores, etc.), lo que facilita la búsqueda y recuperación de información específica sin tener que recorrer toda la estructura.
+ * Esto mejora significativamente el rendimiento de las operaciones de búsqueda, inserción y eliminación en grandes conjuntos de datos.
+ * Ejemplo de uso de multilistas:
+ * Supongamos que tenemos una base de datos de libros en una biblioteca, y queremos organizar los libros por género (ficción, no ficción, ciencia, historia, etc.). Podemos crear una multilista donde cada lista enlazada representa un género específico. Cada nodo en la lista contiene información sobre un libro (título, autor, año de publicación, etc.) y un puntero al siguiente libro en la misma categoría.
+ * Cuando un usuario busca libros de un género específico, podemos acceder directamente a la lista correspondiente sin tener que revisar todos los libros en la base de datos, lo que hace que la búsqueda sea mucho más rápida y eficiente.
+ * programas que utilizan multilistas:
+ * 1. Sistemas de gestión de bases de datos (DBMS): Muchos DBMS utilizan estructuras de datos como multilistas para organizar y acceder a grandes volúmenes de datos almacenados en discos.
+ * 2. Motores de búsqueda: Los motores de búsqueda en Internet utilizan estructuras de datos
+ * como multilistas para indexar y recuperar rápidamente páginas web relevantes basadas en consultas de usuarios.
+ * 3. Sistemas de archivos: Algunos sistemas de archivos utilizan multilistas para gestionar la ubicación y el acceso a archivos almacenados en discos duros.
+ * 
+ */
 //me falta enlace 
