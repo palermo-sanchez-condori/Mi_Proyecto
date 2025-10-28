adb9fbffe7c41a2bb65deade1dbb5645ac26c4d3
palermo-sanchez-condori <palermosanchezcondori@gmail.com>
2025-10-28 09:12:50 -0500

    Actualizar Ejecicio_02.java

--- Trabajo_01/Trabajos_PC3/Ejecicio_02.java
+++ Trabajo_01/Trabajos_PC3/Ejecicio_02.java
@@ -1,6 +1,8 @@
 //concepto
 /*
- * Este es un ejemplo de un archivo Java que podría formar parte de un proyecto de trabajo.
+ * La búsqueda lineal funciona revisando cada elemento de una lista, uno por uno, en orden, hasta encontrar el elemento que buscas.
+Es como buscar un libro específico en un estante: empiezas por el primer libro y miras cada título hasta que encuentras el tuyo o llegas al final del estante. Es simple, pero puede ser lento si la lista es muy larga y el elemento está al final.
+
  */
 // Codigo
 
