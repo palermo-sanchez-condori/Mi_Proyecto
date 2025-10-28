7844e2963528972a0b39d5ed3414a737b42fc037
palermo-sanchez-condori <palermosanchezcondori@gmail.com>
2025-10-28 09:14:37 -0500

    Actualizar Ejecicio_02.java

--- Trabajo_01/Trabajos_PC3/Ejecicio_02.java
+++ Trabajo_01/Trabajos_PC3/Ejecicio_02.java
@@ -3,6 +3,13 @@
  * La búsqueda lineal funciona revisando cada elemento de una lista, uno por uno, en orden, hasta encontrar el elemento que buscas.
 Es como buscar un libro específico en un estante: empiezas por el primer libro y miras cada título hasta que encuentras el tuyo o llegas al final del estante. Es simple, pero puede ser lento si la lista es muy larga y el elemento está al final.
 
+Funciona así:
+​Empieza mirando el elemento que está justo en el medio de la lista.
+​Compara ese elemento con el que buscas:
+​Si es el que buscas, ¡listo!
+​Si el que buscas es menor, descartas toda la mitad derecha de la lista.
+​Si el que buscas es mayor, descartas toda la mitad izquierda.
+​Repites este proceso (ir a la mitad de la sección restante) hasta encontrar el elemento.
  */
 // Codigo
 
