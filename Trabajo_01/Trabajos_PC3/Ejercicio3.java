acedac3badc4780d1769b224118a52e8363cda5f
palermosanchez <palermosanchezcondori@gmail.com>
2025-11-04 13:37:40 -0500

    resursividad

--- Trabajo_01/Trabajos_PC3/Ejercicio3.java
+++ Trabajo_01/Trabajos_PC3/Ejercicio3.java
@@ -1,11 +1,18 @@
 //concepto
 /*
- * Pila de Llamadas de Métodos
- * funcion:
- * 
- * peor tienpo de ejecución: O(n)
- * mejor tiempo de ejecución: O(1)
- * 
+ * Recursividad indirecta:
+ * funciona cuando una función llama a otra función,
+ * y esta a su vez llama a la primera.
+ * Esto crea una cadena de llamadas entre las funciones.
+ * Es útil para resolver problemas que pueden dividirse en subproblemas más pequeños.
+ * Un ejemplo clásico es el cálculo de números pares e impares:
+ * - Una función "esPar" verifica si un número es par.
+ * - Si el número no es par, llama a la función "esImpar".
+ * - La función "esImpar" verifica si un número es impar.
+ * - Si el número no es impar, llama a la función "esPar".
+ * Este proceso continúa hasta que se alcanza una condición base.
+ * Así, ambas funciones trabajan juntas para determinar si un número es par o impar.
+ * La recursividad indirecta permite una mayor flexibilidad en la resolución de problemas.
  */
 //codigo
 
