73d34df3b99f89f1901619e66c1e78e6a06fe04a
palermosanchez <palermosanchezcondori@gmail.com>
2025-11-10 21:29:58 -0500

    concepto nuevo

--- Trabajo_01/Trabajos_PC3/Ejercicio_04.java
+++ Trabajo_01/Trabajos_PC3/Ejercicio_04.java
@@ -1,6 +1,8 @@
-public class Ejercicio_04 {
-    
-}
+
 //concepto
 // enlace del video de la comprovacion.
 // funcionamiento del codigo.
+/*
+ * Ejercicio 04: Crea un programa que solicite al usuario ingresar una cadena de texto y luego
+ * muestre la misma cadena pero con las palabras en orden inverso.
+ */
