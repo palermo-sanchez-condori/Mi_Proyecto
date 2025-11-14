8c59859ff791df7fa37a13aa7c2917f5ef778040
palermosanchez <palermosanchezcondori@gmail.com>
2025-11-13 22:57:37 -0500

    el laberinto

--- Trabajo_01/Trabajos_PC3/Ejercicio_04.java
+++ Trabajo_01/Trabajos_PC3/Ejercicio_04.java
@@ -8,4 +8,21 @@
  * El objetivo principal del backtracking es encontrar todas las soluciones posibles
  * o la mejor solución a un problema dado, evitando explorar caminos que no conducen
  * a soluciones válidas.
+ * 
+ *  Ejemplo del laberinto:
+ *  el backtracking se utiliza para encontrar un camino desde la entrada hasta la salida
+ * del laberinto. Se comienza en la entrada y se exploran todas las direcciones posibles
+ * para avanzar, retrocediendo cuando se encuentra un callejón sin salida.
+ * Si se llega a la salida, se ha encontrado una solución válida.
+ * 
+ * 
+ * seleccion optima es un enfoque utilizado en algoritmos y teoría de la optimización
+ * para seleccionar la mejor opción o conjunto de opciones entre varias alternativas disponibles.
+ * 
+ *  Ejemplo de la mochila:
+ * en el problema de la mochila, se busca maximizar el valor total de los objetos
+ * que se pueden llevar en una mochila con una capacidad limitada.
+ * Se seleccionan los objetos de manera óptima, eligiendo aquellos que proporcionan el mayor valor
+ * sin exceder la capacidad de la mochila.
+ * 
  */
